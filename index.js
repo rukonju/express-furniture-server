bdacd276ebfb0d864edd066dcb6b475356bd6710
rukonju <rukonbdsju@gmail.com>
2021-11-13 19:35:52 +0600

    'test'

--- index.js
+++ index.js
@@ -14,21 +14,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c4wgp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-/* async function verifyToken(req, res, next) {
-    if (req.headers?.authorization?.startsWith('Bearer ')) {
-        const token = req.headers.authorization.split(' ')[1];
-
-        try {
-            const decodedUser = await admin.auth().verifyIdToken(token);
-            req.decodedEmail = decodedUser.email;
-        }
-        catch {
-
-        }
-
-    }
-    next();
-} */
 
 async function run () {
     try{
